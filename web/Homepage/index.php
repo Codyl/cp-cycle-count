42c0b85b443262b888ed7028ce3d66a028ec2859
cody <lil15003@byui.edu>
2020-04-24 21:02:10 -0400

    homepage added

---
+++ web/Homepage/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+    Teacher: Brother Porter
+    Name: Cody Lillywhite
+    Class: cs341
+    Summary: This is my home page for cs341.
+-->
+<html>
+    <head>
+    <meta charset="utf-8" />
+        <meta name="description" content="Web assignment">
+        <meta name="keywords" content="HTML">
+        <meta name="author" content="Cody Lillywhite">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>CHL CS341 Homepage</title>
+    </head>
+    <body>
+        <form action="process.php" mrthod="post">
+            
+            Enter your name!: <input name="data" type="text"/>
+            <input type="submit"/>
+        </form>
+        <?php
+            
+            $age = 21 + 1;
+            $name = "Cody";
+            $interests[] = "this", "that";
+            for()
+            echo $name . " Homepage";
+            echo "Hello ". $visitor .", my name is" . $name . ". I am " . $age . ". My interests are ...";
+            echo '<pre>'; print_r($array); echo '</pre>';
+        ?>
+    </body>
+</html>
