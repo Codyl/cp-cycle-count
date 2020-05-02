e0acb3a71cd982a0ed7263027bba58a52960fde5
cody <lil15003@byui.edu>
2020-05-02 18:46:53 -0400

    shopping

---
+++ web/Shopping-cart/item_fortress_page.php
@@ -0,0 +1,25 @@
+
+<html>
+    <head>
+            <meta charset="utf-8" />
+            <meta name="description" content="Web assignment">
+            <meta name="keywords" content="HTML">
+            <meta name="author" content="Cody Lillywhite">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="browse_style.css">
+            <script src="script.js"></script>
+            <title>CHL CS341 shoppingCart assignment</title>
+            <script src="https://kit.fontawesome.com/yourcode.js"></script>
+            <link rel = "icon" href = "https://fontawesome.com/icons/laptop-code?style=solid" type = "image/x-icon">
+    </head>
+    <body>
+        <h2>Fortress</h2>
+    </body>
+    
+<?php 
+function addToCart(){
+    $_SERVER["cart"] = "something";
+    echo $_SERVER["cart"];
+}
+?>
+</html>
