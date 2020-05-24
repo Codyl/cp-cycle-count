97d9111d4999f50ce67a8d5dcc2f14420299a150
cody <lil15003@byui.edu>
2020-05-24 18:22:18 -0400

    trying fixing To Do's incompleted

--- web/project1/tableDisplay.php
+++ web/project1/tableDisplay.php
@@ -2,8 +2,12 @@
     function displayTable($numToCount){
     require_once "dbAccess.php";
     $db = connectDB();
-    $q = $db->query("SELECT * FROM items ORDER BY name ASC LIMIT {$numToCount}");
-    $items = $q->fetchAll();
+    $whse = "";
+    if($_POST['warehouse'] == "Kentucky") {$whse = "Ky";}
+    elseif($_POST['warehouse'] == "Idaho") {$whse = "Idaho";}
+    else{echo "Invalid warehouse";}
+    
+    
 
     echo "<br><div id='numToCount'>Next {$numToCount} items to Count</div>";
     echo "<table id='itemsTable'>";
@@ -22,81 +26,117 @@
             <th>Update</th>
             <th>Last counted</th>
           </tr>";
-    foreach($items as $i){
-    $q2 = $db->query("SELECT * FROM bins WHERE bin_id = {$i['bin_id']}");
-    $bin = $q2->fetchAll();
-    if($i['counts_id'] != NULL)
-    {
-        $q3 = $db->query("SELECT * FROM counts WHERE counts_id = {$i['counts_id']}");
-        $count = $q3->fetchAll();
-        $writeQtyIO = $count[0]['qty_end']-$count[0]['qty_start'];
-        $totalCost = number_format($i['cost']*$writeQtyIO,2);
-        $countQty = $count[0]['qty_end'];
-        $lastCounted = $count[0]['count_date'];
-    }
-    else
-    {
-        $countQty = 0;
-        $lastCounted = "Never been counted";
-        $writeQtyIO = 0;
-        $totalCost = 0;
-    }
-        echo "<tr>
-            <td>{$i['name']}</td>
-            <td><textarea name='comment' id=''></textarea></td>
-            <td>{$i['qoh']}</td>
-            <td>{$i['qty_avail']}</td>
-            <td>{$i['case_qty']}</td>
-            <td>{$i['case_lyr']}</td>
-            <td><br><input type='text' class='inputData' name='pick' placeholder='Pieces'></td>
-            <td>{$bin[0][3]}:{$bin[0][4]}:{$bin[0][5]}:{$bin[0][6]}:{$bin[0][7]}<br><input type='text' class='inputData' name='cases' placeholder='Cases'></td>
-            <td>$countQty pc</td>
-            <td>$writeQtyIO pc</td>
-            <td>$$totalCost</td>
-            <td><button type='submit' value='Update' id='update'>Update</button></td>
-            <td>$lastCounted</td>
-    </tr>";
+    //Identify details for each item
+//     //Columns for items by warehouse:  item_id serial8 NOT NULL,
+//   name varchar(100) NOT NULL UNIQUE,
+//   cost float8 NOT NULL,
+//   description varchar(100),
+//   qoh int NOT NULL,
+//   qty_avail int NOT NULL,
+//   case_qty int NOT NULL,
+//   case_lyr int NOT NULL,
+        $q = $db->query("SELECT * FROM items{$whse} ORDER BY item_id ASC LIMIT {$numToCount}");
+        $itemsByLoc = $q->fetchAll();
+    foreach($itemsByLoc as $i){
+        //Queries to be used to find item details
+        
+        //Columns for itemsList by warehouse per bin(multiple bins per item):id,item_id, bin_id
+        $qGetItem = $db->query("SELECT * FROM itemList{$whse} WHERE item_id = {$i['item_id']}");
+        $itemLoc = $qGetItem->fetchAll();
+        //Columns for bins by warehouse:
+//             bin_id serial8,
+//   name varchar(12),
+//   is_pick_bin boolean,
+//   area varchar(1),
+//   row int,
+//   rack int,
+//   shelf_lvl int,
+        $q2 = $db->query("SELECT * FROM bins{$whse} WHERE bin_id = {$itemLoc[0]['bin_id']}");
+        $bin = $q2->fetchAll();
+        //Columns for all items in existence at c&p:
+        $q4 = $db->query("SELECT * FROM items WHERE item_id = {$itemLoc[0]['item_id']}");
+        $itemDetails = $q4->fetchAll();
+        //Test the data with var dump
+        //echo var_dump($itemLoc);echo "\n";echo $itemLoc[0]["bin_id"];
+        
+        //Test to ensure there is a existing item count otherwise default to 0's
+        if($i['counts_id'] != NULL)
+        {
+            
+            $q3 = $db->query("SELECT * FROM counts{$whse} WHERE counts_id = {$i['counts_id']}");
+            $count = $q3->fetchAll();
+            $writeQtyIO = $count[0]['qty_end']-$count[0]['qty_start'];
+            $totalCost = number_format($itemDetails[0]['cost']*$writeQtyIO,2);
+            $countQty = $count[0]['qty_end'];
+            $lastCounted = $count[0]['count_date'];
+        }
+        else
+        {
+            $countQty = 0;
+            $lastCounted = "Never been counted";
+            $writeQtyIO = 0;
+            $totalCost = 0;
+        }
+            echo "<tr>
+                <td>{$itemDetails[0]['name']}</td>
+                <td><textarea name='comment' id=''></textarea></td>
+                <td>{$itemDetails[0]['qoh']}</td>
+                <td>{$itemDetails[0]['qty_avail']}</td>
+                <td>{$itemDetails[0]['case_qty']}</td>
+                <td>{$itemDetails[0]['case_lyr']}</td>
+                <td><br><input type='text' class='inputData' name='pick' placeholder='Pieces'></td>
+                <td>";
+                    foreach($bin as $b){
+                        echo "{$b[3]}:{$b[4]}:{$b[5]}:{$b[6]}";
+                        echo "<br><input type='text' class='inputData' name='cases' placeholder='Cases'></td>";
+                    }
+                echo "<td>$countQty pc</td>
+                <td>$writeQtyIO pc</td>
+                <td>$$totalCost</td>
+                <td><button type='submit' value='Update' id='update'>Update</button></td>
+                <td>$lastCounted</td>
+        </tr>";
     }
     echo "</table>";
 }
 ?>
 <script>
     function sortByTh(th){
-        switch(th){
-            case "item":
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case "qoh"://case 1: cannot sort by comments, not a valid sort
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name qoh");
-            case "qty avail":
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case "case":
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case "case lyr":
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case "pick bin":
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case 0:
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case 0:
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case 0:
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case 0:
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            case 0:
-                $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
-                break;
-            default:
-                echo "Unexpected error on attempted sorting method.";
-        }
+        // switch(th){
+        //     case "item":
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY name DESC");
+        //         break;
+        //     case "qoh"://case 1: cannot sort by comments, not a valid sort
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY qoh DESC");
+        //     case "qty avail":
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY qty_avail DESC");
+        //         break;
+        //     case "case":
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY case_qty DESC");
+        //         break;
+        //     case "case lyr":
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY case_lyr DESC");
+        //         break;
+        //     case "pick bin":
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY pick DESC");
+        //         break;
+        //     case 0:
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY whbin DESC");
+        //         break;
+        //     case 0:
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY count DESC");
+        //         break;
+        //     case 0:
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY writein DESC");
+        //         break;
+        //     case 0:
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY cost DESC");
+        //         break;
+        //     case 0:
+        //         $sortedQuery = $db->query("SELECT * FROM items LIMIT {$numToCount} ORDER BY last_counted DESC");
+        //         break;
+        //     default:
+        //         echo "Unexpected error on attempted sorting method.";
+        // }
     }
 </script>
