15fbab8c453d4b7bf33c6ec429d097639b970b5f
cody <lil15003@byui.edu>
2020-06-08 00:10:50 -0400

    later changes

--- web/project1/edit.php
+++ web/project1/edit.php
@@ -1,7 +1,7 @@
 <?php
 
   session_start();
-  if ( ! isset($_SESSION["user_id"])) {
+  if ( ! isset($_SESSION["user_id"]) || $_SESSION["user_id"] != 1) {
     header("Location: /project1/?action=sign-in");
     die();
   }
