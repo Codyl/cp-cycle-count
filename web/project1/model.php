3fcd3217e368276429a5547561e7a8a6d5fbc55a
cody <lil15003@byui.edu>
2020-06-09 18:42:31 -0400

    later changes

--- web/project1/model.php
+++ web/project1/model.php
@@ -52,9 +52,9 @@ function addItemToBin($item,$bin,$whse,$qty){
   
       require_once "../dbAccess.php";
       $db = connectDB();
-      echo "test: SELECT quantity FROM itemsBins WHERE warehouse_id={$whse} AND item_id={$item} AND bin_id={$bin}";
+      //echo "test: SELECT quantity FROM itemBins WHERE warehouse_id={$whse} AND item_id={$item} AND bin_id={$bin}";
       //Check if item is in bin already: if not put it, add to itemsWarehouse, update inventory qty
-      $q = $db->query("SELECT quantity FROM itemsBins WHERE warehouse_id={$whse} AND item_id={$item} AND bin_id={$bin}");
+      $q = $db->query("SELECT quantity FROM itemBins WHERE warehouse_id={$whse} AND item_id={$item} AND bin_id={$bin}");
       $alreadyInBin = $q->fetch();
       
       if($alreadyInBin == null)
