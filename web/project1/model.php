cc0e46cb56d28236dfa12dff08c126d859c36d33
cody <lil15003@byui.edu>
2020-06-09 18:28:06 -0400

    later changes

--- web/project1/model.php
+++ web/project1/model.php
@@ -54,6 +54,7 @@ function addItemToBin($item,$bin,$whse,$qty){
       //Check if item is in bin already: if not put it, add to itemsWarehouse, update inventory qty
       $q = $db->query("SELECT quantity FROM itemsBins WHERE warehouse_id={$whse} AND item_id={$item} AND bin_id={$bin}");
       $alreadyInBin = $q->fetch();
+      echo "test";
       if($alreadyInBin == null)
       {
         $q1 = $db->query("INSERT INTO itemBins (item_id, bin_id, quantity, warehouse_id)
